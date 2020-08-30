eb8aeb6c3de44633f625be687bd9580ae03bf5d9
Anandu B Ajith <reach2anandu@gmail.com>
2020-08-30 14:45:02 +0530

    add function to add a transaction

---
+++ functions/addTransaction.js
@@ -0,0 +1,23 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const db = admin.firestore();
+
+// data -> { groupId , transaction: { from, to amount } }
+const addTransaction = functions.https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+  try {
+    const doc = await db.doc(`group/${data.groupId}`).update({
+      transactions: admin.firestore.FieldValue.arrayUnion(data.transaction),
+    });
+    return doc;
+  } catch (e) {
+    throw new functions.https.HttpsError("internal", e.message);
+  }
+});
+module.exports = addTransaction;
