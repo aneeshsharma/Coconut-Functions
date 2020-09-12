233ad9cd91cec5f844f4f343b874b08893a13039
Anish Sharma <aneeshsharma15@outlook.com>
2020-09-12 14:15:32 +0530

    fixed join group

--- functions/joinGroup.js
+++ functions/joinGroup.js
@@ -1,25 +1,25 @@
-
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 const db = admin.firestore();
 
-const createVisit = functions.https.onCall(async (data, context) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      "failed-precondition",
-      "The function must be called while authenticated."
-    );
-  }
-  try {
-    const doc = await db.doc(`group/${data.groupId}`).update({
-            users: admin.firestore.FieldValue.arrayUnion(
-                data.uid),
-  });
-  return data.groupId;
-}
-   catch (e) {
-    throw new functions.https.HttpsError("internal", e.message);
-  }
+const joinGroup = functions.https.onCall(async (data, context) => {
+    if (!context.auth) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called while authenticated."
+        );
+    }
+    try {
+        const doc = await db.doc(`group/${data.groupId}`).update({
+            users: admin.firestore.FieldValue.arrayUnion(data.uid),
+        });
+        const userDoc = await db.doc(`users/${data.uid}`).update({
+            groups: admin.firestore.FieldValue.arrayUnion(data.groupId),
+        });
+        return data.groupId;
+    } catch (e) {
+        throw new functions.https.HttpsError("internal", e.message);
+    }
 });
-module.exports = createVisit;
+module.exports = joinGroup;
