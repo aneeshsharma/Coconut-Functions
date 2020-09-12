961f27761ac18f68c01616d5a53e7b19308e4949
Anish Sharma <aneeshsharma15@outlook.com>
2020-09-12 20:15:40 +0530

    return group data and user data on join group

--- functions/joinGroup.js
+++ functions/joinGroup.js
@@ -4,20 +4,31 @@ const admin = require("firebase-admin");
 const db = admin.firestore();
 
 const joinGroup = functions.https.onCall(async (data, context) => {
-    if (!context.auth) {
-        throw new functions.https.HttpsError(
-            "failed-precondition",
-            "The function must be called while authenticated."
-        );
-    }
+    // if (!context.auth) {
+    //     throw new functions.https.HttpsError(
+    //         "failed-precondition",
+    //         "The function must be called while authenticated."
+    //     );
+    // }
     try {
-        const doc = await db.doc(`group/${data.groupId}`).update({
+        const groupDoc = db.doc(`group/${data.groupId}`);
+        await groupDoc.update({
             users: admin.firestore.FieldValue.arrayUnion(data.uid),
         });
-        const userDoc = await db.doc(`users/${data.uid}`).update({
+        await db.doc(`users/${data.uid}`).update({
             groups: admin.firestore.FieldValue.arrayUnion(data.groupId),
         });
-        return data.groupId;
+        var groupData = (await groupDoc.get()).data();
+        if (groupData.users && groupData.users.length > 0) {
+            var usersExpanded = [];
+            for (var userId of groupData.users) {
+                const userData = (await db.doc(`users/${userId}`).get()).data();
+                usersExpanded.push(userData);
+            }
+            groupData.users = usersExpanded;
+        }
+        groupData.groupId = data.groupId;
+        return groupData;
     } catch (e) {
         throw new functions.https.HttpsError("internal", e.message);
     }
